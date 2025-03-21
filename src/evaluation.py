5f79ae61faf5565ec15956de42ad663c934c1d00
bogdan.banasiak <bogdan.banasiak.ai>
2025-03-21 10:33:34 +0100

    Add evaluation pipeline

---
+++ src/evaluation.py
@@ -0,0 +1,32 @@
+import asyncio
+
+from ragbits.evaluate.evaluator import Evaluator
+from ragbits.evaluate.dataloaders.local import LocalDataLoader
+from omegaconf import DictConfig, OmegaConf
+
+from evaluation_pipeline import Pipeline
+from config import Config
+
+config = Config()
+
+
+async def main(evaluation_config: DictConfig) -> None:
+    pipeline = Pipeline(evaluation_config.pipeline)
+    pipeline.config = evaluation_config.pipeline
+
+    evaluator = Evaluator()
+    dataloader = LocalDataLoader(
+        path="data/test_pdf.json", split="train", builder="json"
+    )
+
+    results = await evaluator.compute(
+        pipeline=pipeline,
+        dataloader=dataloader,
+        metrics=evaluation_config.metrics,
+    )
+    print(results)
+
+
+if __name__ == "__main__":
+    eval_config = OmegaConf.load("evaluation_config/config_ret.yaml")
+    asyncio.run(main(eval_config))
