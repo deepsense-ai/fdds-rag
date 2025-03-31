20f6a90f5868f22d448922e42c980d291c0b9c97
bogdan.banasiak <bogdan.banasiak.ai>
2025-03-31 10:32:14 +0200

    Add key authentication

--- src/fdds/app.py
+++ src/fdds/app.py
@@ -1,15 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.responses import StreamingResponse
+from fastapi.security import APIKeyHeader
 from pydantic import BaseModel
 from ragbits.core.prompt import ChatFormat
 
+from fdds import config
 from fdds.inference import inference
 
 app = FastAPI()
+api_key_header = APIKeyHeader(name="API-Key")
 
 
 class QueryRequest(BaseModel):
     query_with_history: ChatFormat
+
+
+def get_api_key(api_key: str = Depends(api_key_header)):
+    if api_key != config.API_KEY:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid or missing API Key"
+        )
+    return api_key
 
 
 @app.get("/", summary="Welcome Endpoint", description="Returns a welcome message.")
@@ -23,7 +34,7 @@ async def read_root():
     description="Performs inference based on input query and "
     "optionally provided conversation history",
 )
-async def chat(request: QueryRequest):
+async def chat(request: QueryRequest, api_key: str = Depends(get_api_key)):
     """
     Perform model inference on the input query and provide a streaming response.
 
@@ -34,6 +45,7 @@ async def chat(request: QueryRequest):
     Args:
         request (QueryRequest): The request object containing the query and optional
                                  conversation history.
+        api_key (str): The API key used to authenticate the request.
 
     Returns:
         StreamingResponse: A streaming response yielding real-time chunks of the
